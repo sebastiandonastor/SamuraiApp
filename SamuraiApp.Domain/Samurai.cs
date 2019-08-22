e78414e802c9f39b3e5f3d07e687aa8008967079
Donastor, Sebastian <s.donastor@motorcredito.com.do>
2019-08-22 12:36:24 -0400

    Add project files.

---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public int BattleId { get; set; }
+
+    }
+}
